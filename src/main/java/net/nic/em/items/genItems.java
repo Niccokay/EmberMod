d047f2c48f04b706e3540705aac20e3bdec593b2
Niccokay <134427646+Niccokay@users.noreply.github.com>
2023-06-28 10:18:52 +0200

    General Fixes
    Added Loot tables and fixed names in tags and grouped directories.
    (Prepared Items Example line)

--- src/main/java/net/nic/em/items/genItems.java
+++ src/main/java/net/nic/em/items/genItems.java
@@ -4,10 +4,15 @@ import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 import net.nic.em.EmberMod;
 
 public class genItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, EmberMod.MODID);
+
+
+    // Example: public static final RegistryObject<Item> ITEM = ITEMS.register("item_id", () -> new Item(new Item.Properties()));
+
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
